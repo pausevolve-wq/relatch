6e88c2ecbbc708e8fd48a3100a28b2566353c0d6
pausevolve-wq <pausevolve@gmail.com>
2026-04-18 22:57:06 +0530

    Update ocr.js

--- api/ocr.js
+++ api/ocr.js
@@ -23,7 +23,7 @@ module.exports = async function handler(req, res) {
 
   const dataUri = base64.startsWith('data:') ? base64 : `data:${fileMime};base64,${base64}`;
 
-  if (mistralKey) {
+  if (false && mistralKey) {
     try {
       console.log(`[ocr] trying Mistral OCR for: ${fileName}`);
       
